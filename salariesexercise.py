8be2b4115fa71ac6de9daca83358594b2b3b99b7
Midi Nanakul <midinanakul@hotmail.com>
2022-01-26 14:50:41 -0800

    Answering more questions.

--- salariesexercise.py
+++ salariesexercise.py
@@ -27,3 +27,16 @@ print(salaries[salaries['EmployeeName'] == 'JOSEPH DRISCOLL']['TotalPayBenefits'
 # What's the name of the highest paid person (including benefits)? == NATHANIEL FORD
 print(salaries.sort_values(by='TotalPayBenefits', ascending=False))
 
+# What's the name of the lowest paid person (including benefits)? == Joe Lopez
+print(salaries.iloc[salaries['TotalPayBenefits'].argmin()])
+
+# What was the average (mean) BasePay of all employees per year (2011-2014)?
+print(salaries.groupby('Year').mean()['BasePay'])
+
+# How many unique job titles are there? == 2159
+print(salaries['JobTitle'].nunique())
+
+# What are the top 5 most common jobs? == TO, SN, RN, Pub Aide, Police Office 3
+print(salaries['JobTitle'].value_counts())
+
+# 
