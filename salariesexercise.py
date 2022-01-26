d8d11e2ff153a5e384d23b46055300651a7b70f2
Midi Nanakul <midinanakul@hotmail.com>
2022-01-25 18:43:32 -0800

    Working on solving more problems.

--- salariesexercise.py
+++ salariesexercise.py
@@ -9,9 +9,15 @@ print(salaries)
 # Check how many entries there are. == 148654
 print(salaries.info())
 
-# Calculate the average base pay.
+# Calculate the average base pay. == 66325.45
 salaries['BasePay'].replace({"Not Provided": np.nan}, inplace=True)
 salaries['OvertimePay'].replace({"Not Provided": np.nan}, inplace=True)
 salaries[['BasePay', 'OvertimePay']] = salaries[['BasePay', 'OvertimePay']].replace('', np.nan).astype(float)
 print(salaries['BasePay'].mean())
+
+# Highest amount of overtime pay? == 245131.88
+print(salaries['OvertimePay'].max())
+
+# Job title of JOSEPH DRISCOLL? == CAPTAIN, FIRE SUPPRESSION
+print(salaries[salaries['EmployeeName'] == 'JOSEPH DRISCOLL'])
 
