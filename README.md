4ee663916ec61d0ca7bf58a8b2b93f9cfd682616
Midi Nanakul <midinanakul@hotmail.com>
2022-01-26 16:47:37 -0800

    Working on answering exercise questions.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DS-and-ML
 Learning data science and ML libraries such as numpy, pandas, matplotlib, and more. Will be posting various scripts just to get some practice.
+
+Link for SF Salaries Data set: https://www.kaggle.com/kaggle/sf-salaries
+Link for https://www.kaggle.com/utkarsharya/ecommerce-purchases
--- ecommerceexercise.py
+++ ecommerceexercise.py
@@ -9,3 +9,12 @@ print(ecom.info())
 
 # What is the Average Purchase Price? == 50.35
 print(ecom['Purchase Price'].mean())
+
+# What were the highest and lowest purchase prices?
+# Highest == 99.99
+print(ecom['Purchase Price'].max())
+# Lowest == 0.0
+print(ecom['Purchase Price'].min())
+
+# How many people have 'en' as their language of choice on the website?
+print(ecom[ecom['Language'] == 'en'].count())
