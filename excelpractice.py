d3719a1c763b417a1da00f89c240ae8187d425bc
Nanakul <midinanakul@hotmail.com>
2022-02-04 07:57:00 -0800

    Created new modified excel file from csv file.

--- excelpractice.py
+++ excelpractice.py
@@ -1,3 +1,9 @@
 import pandas as pd
 from openpyxl.workbook import Workbook
 
+df_excel = pd.read_excel('regions.xlsx')
+df_csv = pd.read_csv('Names.csv', header=None)
+df_text = pd.read_csv('data.txt', delimiter='\t')
+
+df_csv.columns = ['First', 'Last', 'Address', 'City', 'State', 'Zip', 'Salary']
+df_csv.to_excel('modified.xlsx')
