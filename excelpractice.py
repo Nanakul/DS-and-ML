88725d88caf17d8bce999c5a28fcb4784861be1e
Nanakul <midinanakul@hotmail.com>
2022-02-04 08:44:47 -0800

    Grabbed specific columns with double bracket, first 3 people with indexing, and a whole person's info with iloc.

--- excelpractice.py
+++ excelpractice.py
@@ -7,3 +7,10 @@ df_text = pd.read_csv('data.txt', delimiter='\t')
 
 df_csv.columns = ['First', 'Last', 'Address', 'City', 'State', 'Zip', 'Salary']
 df_csv.to_excel('modified.xlsx')
+
+print(df_csv[['State', 'Zip']])
+print(df_csv['First'][0:3])
+print(df_csv.iloc[1])
+
+wanted_values = df_csv[['First', 'Last', 'Salary']]
+stored = wanted_values.to_excel('person_salary.xlsx',index=None)
