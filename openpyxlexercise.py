8c62adc4f765ca402196e091370bc438d8faa01b
Nanakul <midinanakul@hotmail.com>
2022-02-04 13:09:42 -0800

    Learning to navigate through data. Created a cell range and set a row range.

--- openpyxlexercise.py
+++ openpyxlexercise.py
@@ -22,7 +22,22 @@ active_sheet = wb2.active
 # Access the value at cell A1
 cell = active_sheet['A1']
 print(cell.value)
-active_sheet['A1'] = 'REGION'
+active_sheet['A1'] = 'Region'
 
 # Save modified excel sheet
 wb2.save('modified_regions.xlsx')
+
+# Learning how to navigate through data
+# Create a range of cells to navigate
+wb = load_workbook('regions.xlsx')
+ws = wb.active
+
+cell_range = ws['A1':'C1']
+col_range = ws['A': 'C']
+print(col_range)
+
+# Set a row range
+row_range = ws[1:5]
+print(row_range)
+
+# Iterate through columns and rows.
