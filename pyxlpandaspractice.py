1704f0a3406f72fbe6e9120f91e7cd37c9b0672a
Nanakul <midinanakul@hotmail.com>
2022-02-07 10:26:27 -0800

    Added new value to column G in the excel worksheet.

--- pyxlpandaspractice.py
+++ pyxlpandaspractice.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from openpyxl import load_workbook
+from openpyxl.styles import Font
 
 df_1 = pd.read_excel('shifts.xlsx', sheet_name='Sheet')
 df_2 = pd.read_excel('shifts.xlsx', sheet_name='Sheet1')
@@ -10,3 +12,12 @@ df_all = pd.concat([df_1, df_2, df_3], sort=False)
 print(df_all)
 
 to_excel = df_all.to_excel('all_shifts.xlsx', index=None)
+
+wb = load_workbook('all_shifts.xlsx')
+ws = wb.active
+
+# Create new column called 'Total' with bold font in column G
+total_col = ws['G1']
+total_col.font = Font(bold=True)
+total_col.value = 'Total'
+
