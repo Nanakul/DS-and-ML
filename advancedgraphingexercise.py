8c68909bb30925f32ea83d0f47c7da8413ca5b80
Nanakul <midinanakul@hotmail.com>
2022-02-07 12:58:38 -0800

    Added Pie chart using the same process as Bar Chart.

--- advancedgraphingexercise.py
+++ advancedgraphingexercise.py
@@ -19,5 +19,13 @@ chart.height, chart.width = 4.75, 20.3
 ws.add_chart(chart, 'B14')
 
 # Add Pie chart from O14 to Q22
+chart2 = PieChart()
+data = Reference(ws, min_col=15, min_row=8, max_col=16, max_row=8)
+labels = Reference(ws, min_col=15, min_row=7, max_col=16, max_row=7)
+chart2.add_data(data, from_rows=True)
+chart2.set_categories(labels)
+chart2.title = '% Counterfeit Crimes'
+chart2.height, chart2.width = 4.75, 6.75
+ws.add_chart(chart2, 'O14')
 
 wb.save('lines.xlsx')
